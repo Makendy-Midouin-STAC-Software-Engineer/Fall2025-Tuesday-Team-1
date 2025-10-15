68edabef3e073d86a932eb0892867554462900a9
AnthonyF <anthonyfontana61@gmail.com>
2025-10-15 15:37:04 -0400

    Add root URL redirect to search page

--- nyc_restaurants/urls.py
+++ nyc_restaurants/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("inspections/", include("inspections.urls")),
+    path('admin/', admin.site.urls),
+    path('inspections/', include('inspections.urls')),
+    path('', RedirectView.as_view(url='inspections/search/', permanent=False), name='home'),
 ]
